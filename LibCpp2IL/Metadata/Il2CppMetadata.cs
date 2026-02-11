abb90ea52315892b46e80e57e5a7cc7480d19454
Cxntrxl <jacksonwood2905@gmail.com>
2026-02-11 13:07:00 +1100

    Checking if it'll just work by increasing the acceptable version range (i am coping)

--- LibCpp2IL/Metadata/Il2CppMetadata.cs
+++ LibCpp2IL/Metadata/Il2CppMetadata.cs
@@ -67,9 +67,9 @@ public class Il2CppMetadata : ClassReadingBinaryReader
         }
 
         var version = BitConverter.ToInt32(bytes, 4);
-        if (version is < 23 or > 31)
+        if (version is < 23 or > 39)
         {
-            throw new FormatException("Unsupported metadata version found! We support 23-31, got " + version);
+            throw new FormatException("Unsupported metadata version found! We support 23-39, got " + version);
         }
 
         LibLogger.VerboseNewline($"\tIL2CPP Metadata Declares its version as {version}");
